b9ef6dfdb36b6a236abfa2e079f5269d99a5b796
Paulo Sergio <pauloserg861@gmail.com>
2022-11-14 20:47:04 -0300

    conectando mongodb e instalando dependencias

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+//mongo db conection - stabelishing connection api when database is up
+mongoose.connect('mongodb://localhost:27017/db-opjs')
+.then(() => {
+		const app = express();
+		const PORT = 3008
 
-const PORT = 3008
+		app.listen(PORT, () => {
+			console.log(`Server is running on port http://localhost:${PORT}`)
+		})
+	})
+	.catch(() => console.log('error conection'))
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port http://localhost:${PORT}`)
-})
