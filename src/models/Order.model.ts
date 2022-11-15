4ce838fa6b3a2084c36a71b7836a9bf644343619
Paulo Sergio <pauloserg861@gmail.com>
2022-11-14 21:10:25 -0300

    criando modelos do banco de dados

---
+++ src/models/Order.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+export const Order = model('order', new Schema({
+	table: {
+		type: String,
+		required:true,
+	},
+	status: { //status receive type enum
+		type: String,
+		enum: ['WAITING','IN_PRODUCTION', 'DONE'],
+		default: 'WAITING',
+		required: true,
+	},
+	// if you don't call the function notation, mongoose will only save the default date when doc is save
+	createdAt: { type: Date, default: Date.now },
+	products: {
+		required: true,
+		type: [{
+			products: {
+				type: Schema.Types.ObjectId,
+				ref: 'product',
+				required: true,
+			},
+		}],
+	},
+	quantity: {
+		type: Number,
+		default: 1,
+	},
+}));
---
+++ src/models/Product.model.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from 'mongoose';
+
+export const Product = model('product', new Schema({
+	name: { type: String, required:true },
+	description: { type: String, required: true },
+	imagePath: { type: String, required: true },
+	pricing: { type: Number, required: true	},
+	//salvando array no mongo
+	ingredient: {
+		required: true,
+		type: {
+			name: { type: String, required:true },
+			icon: { type: String,  required: true },
+		}
+	},
+	category: {
+		required: true,
+		type: Schema.Types.ObjectId,
+		ref: 'category'
+	},
+}));
