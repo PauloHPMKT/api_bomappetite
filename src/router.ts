dff32224b6abc9c835cd4451e87a9c758f93fc38
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-13 21:42:10 -0300

    fix: fix router path

--- src/router.ts
+++ src/router.ts
@@ -40,7 +40,7 @@ router.get('/products', listProducts);
 router.post('/products', upload.single('image'), createProduct);
 
 //get products by category
-router.get('', listProductsByCategories);
+router.get('/categories/:id/products', listProductsByCategories);
 
 //list orders
 router.get('/orders', listOrders);
