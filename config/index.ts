04d81a16c98e2c226cc56df7f08b51014896527c
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-14 13:55:58 -0300

    feat: adicionando apontamentos e configurações

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,8 @@
-import { config } from 'dotenv';
-
 export default {
 	development: {
-
+		DB_STRING_DEV_URI: process.env.DB_STRING_DEV_URI,
+	},
+	production: {
+		DB_STRING_PROD_URI: 'teste'
 	}
 };
