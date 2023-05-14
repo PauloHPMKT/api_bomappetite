1be4cbf36a5ecb33646c310c2a75ca936aa67ae2
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-05-14 14:33:45 -0300

    refactor: isolando middleware de upload do dominio de rotas

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,25 +1,12 @@
-import path from 'node:path';
 import { Router } from "express";
-import multer from 'multer';
 
 import { createProduct } from "../useCases/producties/createProduct";
 import { listProducts } from "../useCases/producties/listProducts";
 import { listProductsByCategories } from '../useCases/categories/listProductsByCategories';
 
-const router = Router();
+import { upload } from '../middlewares/upload-middleware';
 
-// middleware enabling api to receive upload
-const upload = multer({
-	storage: multer.diskStorage({
-		destination(req, file, callback) {
-			callback(null, path.resolve(__dirname, '..', 'uploads'));
-		},
-		filename(req, file, callback) {
-			//salvando imagem com o nome e timestamp
-			callback(null, `${Date.now()}-${file.originalname}`);
-		}
-	})
-});
+const router = Router();
 
 //list products
 router.get('/products', listProducts);
@@ -29,6 +16,5 @@ router.post('/products', upload.single('image'), createProduct);
 
 //get products by category
 router.get('/categories/:id/products', listProductsByCategories);
-
 
 export default router;
