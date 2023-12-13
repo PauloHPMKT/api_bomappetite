839cf697384a1f8baeab762da77b9611a5975592
Paulo Sergio <pauloserg861@gmail.com>
2023-12-13 18:39:35 -0300

    feat: create env config service

---
+++ src/infra/shared/env-config/env-config.interface.ts
@@ -0,0 +1,5 @@
+export interface EnvConfig {
+	getAppPort(): number;
+	getNodeEnv(): string;
+	getDbConnectionEnv(uri: string): string;
+}
---
+++ src/infra/shared/env-config/env-config.service.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+import { join } from "node:path";
+import { EnvConfig } from "./env-config.interface";
+
+export class EnvConfigService implements EnvConfig {
+	constructor(
+		private readonly environment: string,
+		private readonly envFile?: string,
+	) {
+		this.envFile = this.environment === 'production' ? 'env' : `env.${this.environment}`;
+		dotenv.config({
+			path: join(__dirname, `../../../../.${this.envFile}`),
+		});
+	}
+
+	private static instance: EnvConfigService;
+	public static getInstance(environment: string): EnvConfigService {
+		if (!this.instance) return this.instance = new EnvConfigService(environment);
+		return this.instance;
+	}
+
+	getAppPort(): number {
+		return Number(process.env.PORT);
+	}
+	getNodeEnv(): string {
+		return process.env.NODE_ENV ?? this.environment;
+	}
+	getDbConnectionEnv(uri: string): string {
+		return process.env.DB_CONNECTION ?? uri;
+	}
+}
