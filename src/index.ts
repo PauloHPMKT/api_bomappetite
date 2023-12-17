cb68bd8782af8e09cb2d1014705318078194e3c2
Paulo Sergio <pauloserg861@gmail.com>
2023-12-17 16:52:47 -0300

    refactor: define getInstance to call environment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { App } from "./app";
 import { EnvConfigService } from "./infra/shared/env-config/env-config.service";
 
-const envConfigService = new EnvConfigService('development');
+const envConfigService = EnvConfigService.getInstance('development');
 const port = envConfigService.getAppPort();
 
 const app = new App();
