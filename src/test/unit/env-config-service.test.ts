a64fe6d7867e471da38123a451e96e15f7fbfddd
Paulo Sergio <pauloserg861@gmail.com>
2023-12-17 15:04:50 -0300

    feat: implement factory to instance EnvCongif

--- src/test/unit/env-config-service.test.ts
+++ src/test/unit/env-config-service.test.ts
@@ -1,3 +1,10 @@
+import { EnvConfigService } from "../../infra/shared/env-config/env-config.service";
+
+const makeSut = (env?: string) => {
+	const sut = new EnvConfigService(env ?? 'test');
+	return sut;
+}
+
 describe('EnvComfigService', () => {
 	it('should be defined', () => {
 		expect(true).toBeTruthy();
